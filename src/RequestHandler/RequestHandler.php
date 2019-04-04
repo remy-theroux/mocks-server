953ef9ae84049c0d925d0f7b7fb9bf9ead503901
Rémy Théroux <remy.theroux@gmail.com>
2019-04-04 08:13:08 +0200

    feat(Response): Get mock from loaded Mocks amd interpret it

--- src/RequestHandler/RequestHandler.php
+++ src/RequestHandler/RequestHandler.php
@@ -3,29 +3,32 @@ declare(strict_types=1);
 
 namespace Mocks\Server\RequestHandler;
 
+use function Amp\call;
+use Amp\Delayed;
+use Amp\Failure;
 use Amp\Http\Server\Request;
 use Amp\Http\Server\RequestHandler as RequestHandlerInterface;
 use Amp\Http\Server\Response;
-use Amp\Http\Status;
 use Amp\Promise;
 use Amp\Success;
+use Mocks\Server\Exception\NoMockFoundException;
+use Mocks\Server\Models\Mock;
 use Mocks\Server\Models\Mocks;
 use Psr\Log\LoggerInterface;
 
-final class RequestHandler implements RequestHandlerInterface {
-
+final class RequestHandler implements RequestHandlerInterface
+{
     /** @var LoggerInterface */
     private $logger;
 
     /** @var Mocks */
-    private $mocksConfig;
+    private $mocks;
 
-    public function __construct(LoggerInterface $logger, Mocks $mocksConfig)
+    public function __construct(LoggerInterface $logger, Mocks $mocks)
     {
         $this->logger = $logger;
-        $this->mocksConfig = $mocksConfig;
+        $this->mocks = $mocks;
     }
-
 
     /**
      * {@inheritdoc}
@@ -36,14 +39,34 @@ final class RequestHandler implements RequestHandlerInterface {
             'request' => $request,
         ]);
 
-        return new Success(
-            new Response(
-                Status::OK,
-                [
-                    'content-type' => 'text/plain; charset=utf-8',
-                ],
-                'Hello, World!'
-            )
-        );
+        $mocks = $this->filterMocksFromRequest($request);
+        if (0 === count($mocks)) {
+            return new Failure(new NoMockFoundException($request));
+        }
+
+        /** @var Mock $mock */
+        $mock = array_shift($mocks);
+
+        return call(function() use ($mock) {
+            yield new Delayed($mock->getDelay() * 1000);
+
+            return new Success(
+                new Response(
+                    $mock->getStatus(),
+                    [
+                        'content-type' => $mock->getMimeType(),
+                    ],
+                    $mock->getRawBody()
+                )
+            );
+        });
+    }
+
+    private function filterMocksFromRequest(Request $request): ?array
+    {
+        return array_filter($this->mocks->getMocks(), function(Mock $mock) use ($request) {
+            return $mock->getMethod() === $request->getMethod()
+                && $mock->getUri() === $request->getUri()->getPath();
+        });
     }
 }
