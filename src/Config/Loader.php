02cd8e261f12828e08dae4cf53c36f44afc07b21
Rémy Théroux <remy.theroux@gmail.com>
2019-04-04 08:11:06 +0200

    feat(Loader): Load yaml config into DTO object

--- src/Config/Loader.php
+++ src/Config/Loader.php
@@ -3,11 +3,17 @@ declare(strict_types=1);
 
 namespace Mocks\Server\Config;
 
+use Doctrine\Common\Annotations\AnnotationReader;
 use Mocks\Server\Models\Mocks;
 use Psr\Log\LoggerInterface;
 use Symfony\Component\Config\Definition\Processor;
+use Symfony\Component\PropertyInfo\Extractor\PhpDocExtractor;
+use Symfony\Component\Serializer\Mapping\Factory\ClassMetadataFactory;
+use Symfony\Component\Serializer\Mapping\Loader\AnnotationLoader;
 use Symfony\Component\Serializer\NameConverter\CamelCaseToSnakeCaseNameConverter;
 use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
+use Symfony\Component\Serializer\Normalizer\ArrayDenormalizer;
+use Symfony\Component\Serializer\Serializer;
 use Symfony\Component\Yaml\Exception\ParseException;
 use Symfony\Component\Yaml\Yaml;
 
@@ -35,16 +41,27 @@ final class Loader
 
         $this->validateConfig($data);
 
-var_dump($data);
-        $normalizer = new ObjectNormalizer(null, new CamelCaseToSnakeCaseNameConverter());
-        $mocks = $normalizer->denormalize($data['mocks'], Mocks::class);
-var_dump($mocks);
-        return $mocks;
+        return $this->populateMocks($data);
     }
 
     private function validateConfig(array $config): void
     {
         $processor = new Processor();
         $processor->processConfiguration(new MocksServerConfiguration(), $config);
+    }
+
+    private function populateMocks(array $data): Mocks
+    {
+        $objectNormalizer = new ObjectNormalizer(
+            null,
+            new CamelCaseToSnakeCaseNameConverter(),
+            null,
+            new PhpDocExtractor()
+        );
+
+        $arrayDenormalizer = new ArrayDenormalizer();
+        $serializer = new Serializer([$objectNormalizer, $arrayDenormalizer]);
+
+        return $serializer->denormalize($data['mocks'], Mocks::class);
     }
 }
