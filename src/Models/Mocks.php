14d0a527d4a01ec79533e4306bf5ffffeaa7b881
Rémy Théroux <remy.theroux@gmail.com>
2019-05-02 08:06:22 +0200

    refac(Configuration): Delete main mime-type

--- src/Models/Mocks.php
+++ src/Models/Mocks.php
@@ -8,27 +8,18 @@ final class Mocks
     /** @var int */
     private $port;
 
-    /** @var string */
-    private $mimeType;
-
     /** @var Mock[] */
     private $mocks;
 
-    public function __construct(int $port, string $mimeType, array $mocks)
+    public function __construct(int $port, array $mocks)
     {
         $this->port = $port;
-        $this->mimeType = $mimeType;
         $this->mocks = $mocks;
     }
 
     public function getPort(): int
     {
         return $this->port;
-    }
-
-    public function getMimeType(): string
-    {
-        return $this->mimeType;
     }
 
     public function getMocks(): array
